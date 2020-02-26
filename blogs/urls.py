50bb06fe5f14c39c9f71551b18a34853583187e7
ubuntu <drhtka@gmail.com>
2020-02-26 18:27:15 +0000

    index and page posts

--- blogs/urls.py
+++ blogs/urls.py
@@ -3,14 +3,16 @@
 # -*- coding: utf-8 -*-
 
 from django.conf.urls import url, include
-from blogs.views import PostsListView
+from blogs.views import PostsListView, SubscribeListView
 #from blogs.feeds import LatestPostFeed
 
 
-app_name = 'blog_posts'
+#app_name = 'blog_posts'
 
 urlpatterns = [
     url('^list/', PostsListView.as_view(), name='list'),#<str:slug>/
+    url(r'^subscribe/', SubscribeListView.as_view(), name='get')
+    #url('^subscribe/', SubscribeListViewsubs.as_view(), name='subscribe') MyView.as_view()
 
 #    url('^create/$', PostCreateView.as_view(), name='create'),
 #    url(r'^feed/$', LatestPostFeed(), name='post_feed'),
--- blogs/views.py
+++ blogs/views.py
@@ -2,13 +2,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from django.views.generic import ListView, TemplateView
+from django.views.generic import ListView, TemplateView, View
+#from django.views.generic.base import View
+
 from blogs.models import BlogPosts
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 
 
 class IndexTemplateView(TemplateView):
+    #  стартовая траница со всами постами всех пользователей
     def get(self, request):
         template_name = 'blogs/index.html'
         all_posts = BlogPosts.objects.all()
@@ -18,7 +21,7 @@ class IndexTemplateView(TemplateView):
 
 
 class PostsListView(LoginRequiredMixin, ListView):
-
+    #  страница блога зарегестрированного пользователя
     model = BlogPosts
     context_object_name = "posts"
     template_name = 'blogs/details.html'
@@ -29,4 +32,15 @@ class PostsListView(LoginRequiredMixin, ListView):
         qs = super().get_queryset()
         return qs.filter(author=u)
 
-
+class SubscribeListView(LoginRequiredMixin, TemplateView):
+    print('hhhh')
+    # подписка отписка на блоги пользователей
+    model = BlogPosts
+    context_object_name = "subsc"
+    template_name = 'blogs/subscribe.html'
+    login_url = 'subscribe'
+    print('subsc')
+    def get(self, request):
+        print('hello')
+        print(request)
+        return HttpResponse('result')
